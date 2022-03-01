4e5e3ccbc52783c989bcc2be4d5fadfb08585018
Maddela Venkata Krishna Kumar <maddelakrishnakumar@gmail.com>
2022-03-01 09:27:42 +0530

    logger added

--- src/main/java/com/ms/ems/EmployeeservicesApplication.java
+++ src/main/java/com/ms/ems/EmployeeservicesApplication.java
@@ -3,10 +3,14 @@ package com.ms.ems;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class EmployeeservicesApplication {
 
 	public static void main(String[] args) {
+		log.info("Started Building");
 		SpringApplication.run(EmployeeservicesApplication.class, args);
 	}
 
