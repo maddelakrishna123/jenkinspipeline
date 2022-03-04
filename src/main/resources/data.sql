73c13af488b8d075f141b35fcddb70a2c7bbf179
maddelakrishna123 <maddelakrishnakumar@gmail.com>
2022-03-04 05:38:00 +0530

    Update data.sql
    added one more employee

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,4 @@ insert into employee(id,name,gender,age,salary) values(2,'abhishek','Male',5,700
 insert into employee(id,name,gender,age,salary) values(3,'sritam','Male',24,900000);
 insert into employee(id,name,gender,age,salary) values(4,'Veda','FeMale',25,900000);
 insert into employee(id,name,gender,age,salary) values(5,'Nandini','FeMale',26,900000);
+insert into employee(id,name,gender,age,salary) values(6,'Ashish','Male',26,900000);
